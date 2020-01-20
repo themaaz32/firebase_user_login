88328ef78dbffd8a7ac799f33a29ac4a590a7486
themaaz32 <50517157+themaaz32@users.noreply.github.com>
2020-01-20 12:17:32 +0500

    commit

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,7 +1,82 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_user_login/screens/HomeScreen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   final _phoneController = TextEditingController();
+  final _codeController = TextEditingController();
+
+  Future<bool> loginUser(String phone, BuildContext context) async{
+    FirebaseAuth _auth = FirebaseAuth.instance;
+
+    _auth.verifyPhoneNumber(
+        phoneNumber: phone,
+        timeout: Duration(seconds: 60),
+        verificationCompleted: (AuthCredential credential) async{
+          Navigator.of(context).pop();
+
+          AuthResult result = await _auth.signInWithCredential(credential);
+
+          FirebaseUser user = result.user;
+
+          if(user != null){
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) => HomeScreen(user: user,)
+            ));
+          }else{
+            print("Error");
+          }
+
+          //This callback would gets called when verification is done auto maticlly
+        },
+        verificationFailed: (AuthException exception){
+          print(exception);
+        },
+        codeSent: (String verificationId, [int forceResendingToken]){
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Give the code?"),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextField(
+                      controller: _codeController,
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text("Confirm"),
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    onPressed: () async{
+                      final code = _codeController.text.trim();
+                      AuthCredential credential = PhoneAuthProvider.getCredential(verificationId: verificationId, smsCode: code);
+
+                      AuthResult result = await _auth.signInWithCredential(credential);
+
+                      FirebaseUser user = result.user;
+
+                      if(user != null){
+                        Navigator.push(context, MaterialPageRoute(
+                            builder: (context) => HomeScreen(user: user,)
+                        ));
+                      }else{
+                        print("Error");
+                      }
+                    },
+                  )
+                ],
+              );
+            }
+          );
+        },
+        codeAutoRetrievalTimeout: null
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +121,9 @@ class LoginScreen extends StatelessWidget {
                       textColor: Colors.white,
                       padding: EdgeInsets.all(16),
                       onPressed: () {
+                        final phone = _phoneController.text.trim();
+
+                        loginUser(phone, context);
 
                       },
                       color: Colors.blue,
